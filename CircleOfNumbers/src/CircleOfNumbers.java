0d74eef814ed33e48bccb7599cbb1db4f9b615bf
Gudrun <Gudrun@localhost>
2021-05-05 21:30:34 +0200

    Developed skills: puzzles, games.

---
+++ CircleOfNumbers/src/CircleOfNumbers.java
@@ -0,0 +1,30 @@
+/*
+ * Developed skills: puzzles, games.
+ * 
+ * Task:
+ * Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighbouring numbers is equal (note that 0 and n - 1 are neighbouring, too).
+ * Given n and firstNumber/first_number, find the number which is written in the radially opposite position to firstNumber.
+ * 
+ * Input/Output:
+ * [input] integer n
+ * A positive even integer.
+ * Constraints: 4 ≤ n ≤ 1000.
+ * [input] integer firstNumber/first_number
+ * Constraints: 0 ≤ firstNumber ≤ n - 1
+ * [output] an integer
+ */ 
+
+public class CircleOfNumbers {
+    public static int circleOfNumbers(int n, int firstNumber) {
+    	int opposite = 0;
+    	
+    	if ((n % 2 == 0) && (4 <= n && n <= 1000) && ((0 <= firstNumber && firstNumber <= n - 1))) {
+        if (firstNumber >= n/2) {
+          opposite = firstNumber - (n/2);
+        } else if (firstNumber < n/2) {
+          opposite = firstNumber + (n/2);
+        }        
+      }
+    	return opposite;
+    }
+}
--- TakeTenMinuteWalk/src/TenMinWalk.java
+++ TakeTenMinuteWalk/src/TenMinWalk.java
@@ -1,5 +1,5 @@
 /*
- * Developed skills: fundamentas, arrays.
+ * Developed skills: fundamentals, arrays.
  * 
  * You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an 
  * appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk 
