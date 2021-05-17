7e35b24259b70fdfba3b922a67c2e571ce4988f0
Gudrun <Gudrun@localhost>
2021-05-17 21:00:31 +0200

    Developed skills: fundamentals.

--- TimeConverter/src/timeConverter.java
+++ TimeConverter/src/timeConverter.java
@@ -16,12 +16,9 @@
  */
 
 public class timeConverter{
-  public static String toTime(int seconds){
-    int hours;
-    int minutes = 0;
-    
-    hours = seconds / 3600;
-    minutes = (seconds - (hours * 3600)) / 60;
+  public static String toTime(int seconds){  
+    int hours = seconds / 3600;
+    int minutes = (seconds - (hours * 3600)) / 60;
     
     String output = hours + " hour(s) and " + minutes + " minute(s)";
         
