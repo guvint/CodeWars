fec5995d17b7d43aa8eb1366f8af2fb230a95f2d
Gudrun Søborg Vinther <gudrun.vinther@gmail.com>
2021-05-23 20:05:15 +0200

    updated my email

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,5 +1,5 @@
 /*
- * Developed skills: fundamentals.
+ * Developed skills: fundamentals. 
  * 
  * Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
  */
