87f9281dc3fbe3123bd9be0b70095175f8dd6961
Gudrun <Gudrun@localhost>
2021-05-23 12:16:32 +0200

    Developed skills: fundamentals.

---
+++ Solution/src/Solution.java
@@ -0,0 +1,11 @@
+/*
+ * Developed skills: fundamentals.
+ * 
+ * Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+ */
+
+class Solution {
+	static String removeExclamationMarks(String s) {
+		return s.replace("!", "");
+	}
+}
